44c8ee42f0cef6739a69739a442d082d79408616
Endy Muhardin <endy.muhardin@gmail.com>
2025-05-28 08:57:10 +0700

    skeleton code stack dan queue

---
+++ queue/Antrian.java
@@ -0,0 +1,17 @@
+public class Antrian {
+    public void tambahPengantri(Pembeli p){
+
+    }
+
+    public Pembeli layani(){
+        return null;
+    }
+
+    public Pembeli terdepan(){
+        return null;
+    }
+
+    public Boolean habiskah(){
+        return false;
+    }
+}
---
+++ stack/Tumpukan.java
@@ -0,0 +1,17 @@
+public class Tumpukan {
+    public void tumpuk(Barang b){
+
+    }
+
+    public Barang ambil(){
+        return null;
+    }
+
+    public Barang lihatPalingAtas(){
+        return null;
+    }
+
+    public Boolean kosongkah(){
+        
+    }
+}
