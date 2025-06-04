5fd917abb752aef41bc7e06f308abb665053cfcd
Endy Muhardin <endy.muhardin@gmail.com>
2025-06-04 08:30:09 +0700

    keterangan method queue

--- queue/Antrian.java
+++ queue/Antrian.java
@@ -1,16 +1,21 @@
 public class Antrian {
+
+    // push
     public void tambahPengantri(Pembeli p){
 
     }
 
+    // pop
     public Pembeli layani(){
         return null;
     }
 
+    // peek
     public Pembeli terdepan(){
         return null;
     }
 
+    // isEmpty
     public Boolean habiskah(){
         return false;
     }
